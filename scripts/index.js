98b2962944f9b1c6e72a89434b65f6a7e8edbf65
Slava Garanovich <founder@intgame.ru>
2022-06-28 22:46:02 +0300

    hotfix:  сделать правки второго ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,23 @@
-let formEdit = document.querySelector('.popup');
-let formEditContainer = formEdit.querySelector('.popup__container');
+let popupEdit = document.querySelector('.popup');
+let formEditContainer = popupEdit.querySelector('.popup__container');
 let btnEdit =  document.querySelector('.profile__edit-button');
 let btnClose = document.querySelector('.popup__close');
 let inputName = formEditContainer.querySelector('.popup__input_type_name');
 let inputStatus = formEditContainer.querySelector('.popup__input_type_status');
 let btnSave = formEditContainer.querySelector('.popup__save');
 let profile = document.querySelector('.profile');
+let formEdit = formEditContainer.querySelector('.popup__form');
 
 function openFormEdit() {
   let profileName = profile.querySelector('.profile__name');
   let profileStatus = profile.querySelector('.profile__status');
   inputName.value= profileName.textContent;
   inputStatus.value = profileStatus.textContent;
-  formEdit.classList.add('popup_opened');
+  popupEdit.classList.add('popup_opened');
  }
 
 function closeFormEdit() {
-  formEdit.classList.remove('popup_opened');
+  popupEdit.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
@@ -30,4 +31,4 @@ function formSubmitHandler(evt) {
 
 btnEdit.addEventListener('click', openFormEdit);
 btnClose.addEventListener('click', closeFormEdit);
-formEditContainer.addEventListener('submit', formSubmitHandler);
+formEdit.addEventListener('submit', formSubmitHandler);
